eff62f0f9c8b8a44b74e58813c63d77680583f4a
Marius <marius.muntean@live.com>
2018-11-03 10:02:02 +0100

    Added a new sample: custom on click handler for Legend Item

--- samples/ChartJs.Blazor.Sample/wwwroot/SampleJsInterop.js
+++ samples/ChartJs.Blazor.Sample/wwwroot/SampleJsInterop.js
@@ -6,5 +6,20 @@ window.SampleFunctions = {
     CustomOnHoverFunc: function(event, item) {
         console.log(event);
         console.log(item);
+    },
+    HideOtherDatasetsFunc: function(e, legendItem) {
+        var index = legendItem.datasetIndex;
+        var ci = this.chart;
+
+        for (var i = 0; i < ci.config.data.datasets.length; i++) {
+
+            if (i !== index) {
+                var meta = ci.getDatasetMeta(i);
+                meta.hidden = meta.hidden === null ? !ci.data.datasets[i].hidden : null;
+            }
+        }
+
+        // We hid a some datasets ... re-render the chart
+        ci.update();
     }
 };
