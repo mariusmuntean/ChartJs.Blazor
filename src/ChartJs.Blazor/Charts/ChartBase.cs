70ee41e35b5e1385ed380be5048f7c7b5f0f926e
Marius <marius.muntean@live.com>
2019-04-06 18:34:35 +0200

    1. The properties of ChartBase aren't publicly settable
    2. Accepted ReSharper's suggestion for fixing namespaces (though it built just fine without it :D)

--- src/ChartJs.Blazor/Charts/ChartBase.cs
+++ src/ChartJs.Blazor/Charts/ChartBase.cs
@@ -8,13 +8,13 @@ namespace ChartJs.Blazor.Charts
     public abstract class ChartBase<TConfig> : ComponentBase where TConfig : ChartConfigBase
     {
         [Inject] protected IJSRuntime JsRuntime { get; set; }
-        
-        [Parameter] public TConfig Config { get; private set; }
-        
-        [Parameter] public int Width { get; private set; } = 400;
 
-        [Parameter] public int Height { get; private set; } = 400;
-        
+        [Parameter] protected TConfig Config { get; set; }
+
+        [Parameter] protected int Width { get; set; } = 400;
+
+        [Parameter] protected int Height { get; set; } = 400;
+
         protected override void OnAfterRender()
         {
             try
@@ -22,7 +22,9 @@ namespace ChartJs.Blazor.Charts
                 base.OnAfterRender();
                 JsRuntime.SetupChart(Config);
             }
-            catch { } // https://github.com/aspnet/AspNetCore/issues/8327
+            catch
+            {
+            } // https://github.com/aspnet/AspNetCore/issues/8327
         }
 
         public void Update()
