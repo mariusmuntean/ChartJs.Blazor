63930aba5eef0d2e353ea30a64f5a2b309d9f9bf
Lars Glud <larshg@gmail.com>
2019-04-09 10:15:19 +0200

    Don't add wrong generatelabelsfunc.

--- src/ChartJs.Blazor/dist/ChartJsInterop.js
+++ src/ChartJs.Blazor/dist/ChartJsInterop.js
@@ -95,13 +95,8 @@ function WireUpGenerateLabelsFunc(config) {
         config.options.legend.labels.generateLabels.includes(".")) {
         var generateLabelsNamespaceAndFunc = config.options.legend.labels.generateLabels.split(".");
         var generateLabelsFunc = window[generateLabelsNamespaceAndFunc[0]][generateLabelsNamespaceAndFunc[1]];
-        if (typeof generateLabels === "function") {
+        if (typeof generateLabels === "function")
             config.options.legend.labels.generateLabels = generateLabelsFunc;
-        } else { // fallback to the default
-            config.options.legend.labels.generateLabels = Chart.defaults.global.legend.labels.generateLabels;
-        }
-    } else { // fallback to the default
-        config.options.legend.labels.generateLabels = Chart.defaults.global.legend.labels.generateLabels;
     }
 }
 
