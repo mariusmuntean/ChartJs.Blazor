2c3a5b562e7dde25aae5acc8e9849c0d0fe45643
jli103828 <39618498+jli103828@users.noreply.github.com>
2019-06-22 22:14:45 +0200

    Start with timeaxis

---
+++ ChartJs.Blazor/ChartJS/Common/Enums/TimeDistribution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChartJs.Blazor.ChartJS.Common.Enums
+{
+    class TimeDistribution : StringEnum
+    {
+        public static TimeDistribution Linear => new TimeDistribution("linear");
+        public static TimeDistribution Series => new TimeDistribution("series");
+
+        private TimeDistribution(string stringRep) : base(stringRep) { }
+    }
+}
