d4afc5b9891575ba0e90df66cb2907615a03daec
SeppPenner <HansM2013@gmx.de>
2019-08-07 16:53:30 +0200

    Adjusted comment in doughnut dataset: Removed pie chart comment.

--- ChartJs.Blazor/ChartJS/DoughnutChart/DoughnutDataset.cs
+++ ChartJs.Blazor/ChartJS/DoughnutChart/DoughnutDataset.cs
@@ -59,7 +59,7 @@ namespace ChartJs.Blazor.ChartJS.DoughnutChart
 
         /// <summary>
         /// Gets or sets the relative thickness of the dataset.
-        /// Providing a value for weight will cause the pie or doughnut dataset to be drawn with a thickness relative to the sum of all the dataset weight values.
+        /// Providing a value for weight will cause the doughnut dataset to be drawn with a thickness relative to the sum of all the dataset weight values.
         /// </summary>
         public int Weight { get; set; } = 1;
 
