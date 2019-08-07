ec1ef0d5be2efe1cd83abfe3aeac1f2876eb09ca
SeppPenner <HansM2013@gmx.de>
2019-08-07 15:55:23 +0200

    Removed dougnut title, updated pie chart sample to work with List<double>, fixed dataset according to documentation page.

--- ChartJs.Blazor/ChartJS/DoughnutChart/DoughnutDataset.cs
+++ ChartJs.Blazor/ChartJS/DoughnutChart/DoughnutDataset.cs
@@ -8,11 +8,13 @@ namespace ChartJs.Blazor.ChartJS.DoughnutChart
     /// </summary>
     public class DoughnutDataset
     {
+        // Todo: Is this really needed? It's already configured in the doughnut chart config?
         /// <summary>
         /// Gets or sets the chart type. "Doughnut" in this case.
         /// </summary>
         public ChartTypes Type { get; } = ChartTypes.Doughnut;
 
+        // Todo: Is this really needed?
         /// <summary>
         /// Gets or sets the label of the dataset.
         /// </summary>
@@ -22,6 +24,13 @@ namespace ChartJs.Blazor.ChartJS.DoughnutChart
         /// Gets or sets the fill color of the arcs in the dataset.
         /// </summary>
         public string[] BackgroundColor { get; set; }
+
+        // Todo: Make this an enum later?!
+        /// <summary>
+        /// Gets or sets the border align. When 'center' is set, the borders of arcs next to each other will overlap.
+        /// When 'inner' is set, it is guaranteed that all the borders are not overlap.
+        /// </summary>
+        public string BorderAlign { get; set; } = "center";
 
         /// <summary>
         /// Gets or sets the border color of the arcs in the dataset.
@@ -47,6 +56,12 @@ namespace ChartJs.Blazor.ChartJS.DoughnutChart
         /// Gets or sets the stroke width of the arcs when hovered.
         /// </summary>
         public int[] HoverBorderWidth { get; set; }
+
+        /// <summary>
+        /// Gets or sets the relative thickness of the dataset.
+        /// Providing a value for weight will cause the pie or doughnut dataset to be drawn with a thickness relative to the sum of all the dataset weight values.
+        /// </summary>
+        public int Weight { get; set; } = 1;
 
         /// <summary>
         /// Gets or sets the data in the dataset.
