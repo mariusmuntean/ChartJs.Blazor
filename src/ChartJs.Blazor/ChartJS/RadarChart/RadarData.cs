745b156865273d94bed12a5287f6f79a7c5b24e3
Marius <marius.muntean@live.com>
2019-10-26 10:40:49 +0200

    Added a RadarChart sample

--- src/ChartJs.Blazor/ChartJS/RadarChart/RadarData.cs
+++ src/ChartJs.Blazor/ChartJS/RadarChart/RadarData.cs
@@ -4,8 +4,8 @@ namespace ChartJs.Blazor.ChartJS.RadarChart
 {
     public class RadarData
     {
-        public List<string> Labels { get; set; }
+        public List<string> Labels { get; set; } = new List<string>();
 
-        public List<RadarDataset> Datasets { get; set; }   
+        public List<RadarDataset> Datasets { get; set; }   = new List<RadarDataset>();
     }
 }
