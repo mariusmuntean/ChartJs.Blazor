73f227c0139ace0fea8add031f34a387e6b663fa
Marius <marius.muntean@live.com>
2019-11-02 16:38:55 +0100

    Downgraded library dependencies o latest STABLE Blazor

--- samples/ServerSide/ChartJs.Blazor.Sample.ServerSide/Program.cs
+++ samples/ServerSide/ChartJs.Blazor.Sample.ServerSide/Program.cs
@@ -1,13 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+using ChartJs.Blazor.Sample.ServerSide;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace ChartJs.Blazor.Sample.ServerSide
 {
