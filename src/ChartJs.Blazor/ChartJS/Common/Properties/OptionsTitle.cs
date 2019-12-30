c76b41fdd8f65d6ab7b9cacee4a573e67701abf2
Joelius300 <52202086+Joelius300@users.noreply.github.com>
2019-12-30 19:19:49 +0100

    Add properties to OptionTitle, showcase in Test and BubbleChart

--- src/ChartJs.Blazor/ChartJS/Common/Properties/OptionsTitle.cs
+++ src/ChartJs.Blazor/ChartJS/Common/Properties/OptionsTitle.cs
@@ -1,18 +1,58 @@
-﻿namespace ChartJs.Blazor.ChartJS.Common.Properties
+﻿using ChartJs.Blazor.ChartJS.Common.Enums;
+using ChartJs.Blazor.Util;
+
+namespace ChartJs.Blazor.ChartJS.Common.Properties
 {
     /// <summary>
-    /// Represents the title of a chart
+    /// The title-subconfig of <see cref="BaseConfigOptions"/>. Specifies how the chart title is displayed.
     /// </summary>
     public class OptionsTitle
     {
         /// <summary>
-        /// Determines if the title should be displayed or not
+        /// Gets or sets a value indicating whether the title should be displayed or not.
         /// </summary>
         public bool Display { get; set; }
 
         /// <summary>
-        /// The actual title to display
+        /// Gets or sets the position of the title.
         /// </summary>
-        public string Text { get; set; }
+        public Position Position { get; set; }
+
+        /// <summary>
+        /// Gets or sets the font size for the title text.
+        /// </summary>
+        public int FontSize { get; set; } = 12;
+
+        /// <summary>
+        /// Gets or sets the font family for the title text.
+        /// </summary>
+        public string FontFamily { get; set; }
+
+        /// <summary>
+        /// Gets or sets the font color for the title text.
+        /// <para>See <see cref="ColorUtil"/> for working with colors.</para>
+        /// </summary>
+        public string FontColor { get; set; }
+
+        /// <summary>
+        /// Gets or sets the font style for the title text.
+        /// </summary>
+        public FontStyle FontStyle { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of pixels to add above and below the title text.
+        /// </summary>
+        public int Padding { get; set; } = 10;
+
+        /// <summary>
+        /// Gets or sets the height of an individual line of text.
+        /// <para>As per documentation here https://developer.mozilla.org/en-US/docs/Web/CSS/line-height </para>
+        /// </summary>
+        public double LineHeight { get; set; } = 1.2;
+
+        /// <summary>
+        /// Gets or sets the title text to display. If specified as an array, text is rendered on multiple lines.
+        /// </summary>
+        public IndexableOption<string> Text { get; set; }
     }
 }
