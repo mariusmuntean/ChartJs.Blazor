13c58d5458f76a1b25c44e88f4681df1f7850d30
Dan Chaney <dan.chaney@outlook.com>
2020-01-23 13:59:57 -0600

    Adding adnimation duration for the barchart options.  Also added duration to the arcanimation property of a pir chart.

--- src/ChartJs.Blazor/ChartJS/BarChart/BarOptions.cs
+++ src/ChartJs.Blazor/ChartJS/BarChart/BarOptions.cs
@@ -1,16 +1,22 @@
 ﻿using ChartJs.Blazor.Charts;
 using ChartJs.Blazor.ChartJS.Common;
+using ChartJs.Blazor.ChartJS.Common.Properties;
 
 namespace ChartJs.Blazor.ChartJS.BarChart
 {
-    /// <summary>
-    /// The options-subconfig of a <see cref="BarConfig"/>.
-    /// </summary>
-    public class BarOptions : BaseConfigOptions
-    {
-        /// <summary>
-        /// Gets or sets the scales for the <see cref="ChartJsBarChart"/>.
-        /// </summary>
-        public BarScales Scales { get; set; }
-    }
+	/// <summary>
+	/// The options-subconfig of a <see cref="BarConfig"/>.
+	/// </summary>
+	public class BarOptions : BaseConfigOptions
+	{
+		/// <summary>
+		/// General animation time
+		/// </summary>
+		public Animation Animation { get; set; }
+
+		/// <summary>
+		/// Gets or sets the scales for the <see cref="ChartJsBarChart"/>.
+		/// </summary>
+		public BarScales Scales { get; set; }
+	}
 }
