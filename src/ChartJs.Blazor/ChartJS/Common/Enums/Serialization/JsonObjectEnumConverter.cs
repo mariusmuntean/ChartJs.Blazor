73cba5fdcbd567c718e23b3c2d2f7c0231b2eb1b
Joelius300 <52202086+Joelius300@users.noreply.github.com>
2020-03-03 11:42:14 +0100

    Begin deserialization support for ObjectEnum

---
+++ src/ChartJs.Blazor/ChartJS/Common/Enums/Serialization/JsonObjectEnumConverter.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Concurrent;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace ChartJs.Blazor.ChartJS.Common.Enums.Serialization
+{
+    /* Currently supported token/types:
+     * Integer -> int
+     * Float -> double
+     * String -> string
+     * Boolean -> bool
+     * Null -> null
+     * Undefined -> null
+     */
+    internal class JsonObjectEnumConverter : JsonConverter<ObjectEnum>
+    {
+        private const string NotSupportedTokenMessage = "Deserializing ObjectEnums from token type '{0}' isn't supported. " +
+                                                        "Supported tokens: {1}";
+
+        private static readonly JsonToken[] s_supportedTokens = new[]
+        {
+            JsonToken.Null, JsonToken.Undefined, JsonToken.Boolean, JsonToken.Float, JsonToken.Integer, JsonToken.String
+        };
+
+        public override ObjectEnum ReadJson(JsonReader reader, Type objectType, [AllowNull] ObjectEnum existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            if (s_supportedTokens.Contains(reader.TokenType))
+            {
+                // the supported tokens are all primitive so we can just do it like this. See similar use case:
+                // https://github.com/JamesNK/Newtonsoft.Json/blob/a31156e90a14038872f54eb60ff0e9676ca4a0d8/Src/Newtonsoft.Json/Converters/ExpandoObjectConverter.cs#L81
+                ObjectEnumFactory factory = ObjectEnumFactory.GetFactory(objectType);
+                // TODO Check what c# types those token types produce and act accordingly here and in the 
+                // summary of ObjectEnum. Maybe it's not double but float.
+                return factory.Create(reader.Value);
+            }
+
+            throw new NotSupportedException(string.Format(NotSupportedTokenMessage, reader.TokenType, string.Join(", ", s_supportedTokens)));
+        }
+
+        public override void WriteJson(JsonWriter writer, ObjectEnum wrapper, JsonSerializer serializer)
+        {
+            try
+            {
+                // TODO check if it's one of the supported types. Otherwise it might get serialized but can't be deserialized.
+                writer.WriteValue(wrapper.Value);
+            }
+            catch (JsonWriterException)
+            {
+                // if the contained value can't be written in a single Token, deserialization isn't supported
+                // so we won't support serializing it either.
+                throw new NotSupportedException($"The type '{wrapper.Value.GetType().FullName}' isn't supported " +
+                    $"in object enums because it cannot be written as a single token.");
+            }
+        }
+    }
+}
---
+++ src/ChartJs.Blazor/ChartJS/Common/Enums/Serialization/ObjectEnumFactory.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ChartJs.Blazor.ChartJS.Common.Enums.Serialization
+{
+    internal class ObjectEnumFactory
+    {
+        private static readonly ConcurrentDictionary<Type, ObjectEnumFactory> s_factorySingletons = new ConcurrentDictionary<Type, ObjectEnumFactory>();
+        
+        private readonly Lazy<Dictionary<Type, ConstructorInfo>> _constructorCache;
+        protected Dictionary<Type, ConstructorInfo> ConstructorCache => _constructorCache.Value;
+
+        private readonly Type _enumType;
+
+        public static ObjectEnumFactory GetFactory(Type enumType)
+        {
+            if (enumType == null)
+                throw new ArgumentNullException(nameof(enumType));
+
+            if (!typeof(ObjectEnum).IsAssignableFrom(enumType))
+                throw new ArgumentException($"The type '{enumType.FullName}' doesn't inherit from '{typeof(ObjectEnum).FullName}'");
+
+            return s_factorySingletons.GetOrAdd(enumType, type => new ObjectEnumFactory(type));
+        }
+
+        private ObjectEnumFactory(Type enumType)
+        {
+            // checks omitted because the constructor is non-public
+            _enumType = enumType;
+            _constructorCache = new Lazy<Dictionary<Type, ConstructorInfo>>(CreateConstructorDictionary);
+        }
+
+        public ObjectEnum Create(object value)
+        {
+            if (ConstructorCache.TryGetValue(value.GetType(), out ConstructorInfo constructor))
+            {
+                return (ObjectEnum)constructor.Invoke(new[] { value });
+            }
+            else
+            {
+                throw new NotSupportedException($"The object enum '{_enumType.FullName}' doesn't have a constructor which takes a single " +
+                                                $"argument of type '{value.GetType().FullName}'.");
+            }
+        }
+
+        private Dictionary<Type, ConstructorInfo> CreateConstructorDictionary()
+        {
+            var dict = new Dictionary<Type, ConstructorInfo>();
+            ConstructorInfo[] constructors = _enumType.GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic);
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                ParameterInfo[] constructorParams = constructor.GetParameters();
+                if (constructorParams.Length != 1)
+                {
+                    continue;
+                }
+
+                Type paramType = constructorParams[0].ParameterType;
+                if (paramType != typeof(object))
+                {
+                    dict.Add(paramType, constructor);
+                }
+            }
+
+            return dict;
+        }
+    }
+}
