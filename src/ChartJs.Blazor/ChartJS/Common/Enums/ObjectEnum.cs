4aa134195a2e05ab14e47ff360b48bad9db14ea5
Joelius300 <52202086+Joelius300@users.noreply.github.com>
2020-03-19 21:15:18 +0100

    Refactor StringEnum and ObjectEnum
    Fix equality stuff, improve and add summaries, prevent recursive ObjectEnums

--- src/ChartJs.Blazor/ChartJS/Common/Enums/ObjectEnum.cs
+++ src/ChartJs.Blazor/ChartJS/Common/Enums/ObjectEnum.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ChartJs.Blazor.ChartJS.Common.Enums
@@ -6,7 +7,7 @@ namespace ChartJs.Blazor.ChartJS.Common.Enums
     /// <summary>
     /// The base class for enums that can represent different types. We also use these
     /// "enums" like Discriminated Unions to provide a type safe way of communicating with the
-    /// dynamic language javascript is.
+    /// dynamic language JavaScript is.
     /// <para>
     /// De-/serialization is supported but only for the following types:
     /// <see cref="int"/>, <see cref="double"/>, <see cref="string"/> and <see cref="bool"/>.
@@ -17,18 +18,22 @@ namespace ChartJs.Blazor.ChartJS.Common.Enums
     /// When implementing an object enum, make sure to provide only private constructors
     /// with the types that are allowed (DO NOT expose public constructors; expose meaningful
     /// static factory methods instead). The actual enum values are static properties that pass
-    /// the correct value to the private constructor. You can also have static factory methods that
+    /// the correct value to the private constructor. Make these properties return new values
+    /// everytime so we don't create all the enum values even though we don't use them.
+    /// In the classic use case, we don't call many of these properties anyway and usually
+    /// only a few times. You can also have static factory methods that
     /// create an instance of the object enum with the specified value as long as the parameter
     /// type is supported. Also consider sealing your enum unless you have a specific reason not to.
     /// </para>
     /// </summary>
     [Newtonsoft.Json.JsonConverter(typeof(Serialization.JsonObjectEnumConverter))]
-    public abstract class ObjectEnum
+    public abstract class ObjectEnum : IEquatable<ObjectEnum>
     {
         /// <summary>
-        /// Gets the types that are supported for serialization and deserialization.
-        /// <see cref="ObjectEnum"/> can contain objects of different types but you will get
-        /// a <see cref="NotSupportedException"/> once you try to serialize or deserialize that object.
+        /// Gets the <see cref="Type"/>s that are supported for serialization and deserialization.
+        /// <see cref="ObjectEnum"/> can contain objects of different types but you will get a
+        /// <see cref="NotSupportedException"/> once you try to serialize (or deserialize) that
+        /// <see cref="ObjectEnum"/>.
         /// </summary>
         private static readonly Type[] SupportedSerializationTypes = new[]
         {
@@ -41,54 +46,80 @@ namespace ChartJs.Blazor.ChartJS.Common.Enums
         internal object Value { get; }
 
         /// <summary>
-        /// Creates a new instance of <see cref="ObjectEnum"/> with a value.
+        /// Creates a new instance of <see cref="ObjectEnum"/>.
         /// </summary>
-        /// <param name="value">The value this enum-instance is supposed to represent.</param>
+        /// <param name="value">The value this instance is supposed to represent.</param>
         protected ObjectEnum(object value)
         {
             Value = value ?? throw new ArgumentNullException(nameof(value));
+
+            if (value is ObjectEnum)
+                throw new ArgumentException("The value cannot be an ObjectEnum. " +
+                                            "Recursive ObjectEnums aren't allowed.");
         }
 
         /// <summary>
-        /// Checks if a <see cref="Type"/> is in the list of supported serialization types. If
-        /// this function returns <see langword="false"/>, de-/serialization will fail on <see cref="ObjectEnum"/>s
-        /// containing an instance of that <see cref="Type"/> (<paramref name="type"/>).
+        /// Checks if a <see cref="Type"/> is in the list of supported serialization types.
+        /// If this function returns <see langword="false"/>, de-/serialization will fail on
+        /// <see cref="ObjectEnum"/>s containing an instance of that <see cref="Type"/>
+        /// (<paramref name="type"/>).
         /// </summary>
         /// <param name="type">The <see cref="Type"/> to check.</param>
-        internal static bool IsSupportedSerializationType(Type type) => SupportedSerializationTypes.Contains(type);
+        internal static bool IsSupportedSerializationType(Type type) =>
+            SupportedSerializationTypes.Contains(type);
 
         /// <summary>
-        /// Returns the string representation of the underlying object. Calls <see cref="object.ToString"/>
-        /// on the underlying object.
+        /// Determines whether the specified object is considered equal to the current object.
+        /// <para>
+        /// <paramref name="obj"/> is considered to be equal to this instance if it..
+        /// <list type="bullet">
+        /// <item>is the same instance as this instance.</item>
+        /// <item>is another <see cref="ObjectEnum"/> with the same internal value.</item>
+        /// <item>is the same value as the internal value of this <see cref="ObjectEnum"/>.</item>
+        /// </list>
+        /// </para>
         /// </summary>
-        /// <returns>The string representation of the underlying object.</returns>
-        public override string ToString() => Value.ToString();
-
-        // TODO: Check how to properly do equality stuff for this wrapper class
-        // https://softwareengineering.stackexchange.com/questions/397235/should-wrappers-compare-as-equal-using-the-operator-when-they-wrap-the-same-o
-
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-        public static bool operator == (ObjectEnum a, ObjectEnum b) => a.Value == b.Value;
-        public static bool operator != (ObjectEnum a, ObjectEnum b) => a.Value != b.Value;
-#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
-
-        /// <summary>
-        /// Determines whether the specified object instance is considered equal to the current instance.
-        /// </summary>
-        /// <param name="obj">The object to compare with.</param>
-        /// <returns>true if the objects are considered equal; otherwise, false.</returns>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns>true if the specified object is considered to be equal to the current object;
+        /// otherwise, false.</returns>
         public override bool Equals(object obj)
         {
-            if (typeof(ObjectEnum).IsAssignableFrom(obj.GetType()) && obj != null)
-                return Value.Equals(((ObjectEnum)obj).Value);
+            if (obj is ObjectEnum asEnum)
+            {
+                return Equals(asEnum);
+            }
 
-            return false;
+            return Value.Equals(obj);
         }
 
         /// <summary>
-        /// Returns the hash of the underlying object.
+        /// Indicates whether the current object is equal to another object of the same type.
         /// </summary>
-        /// <returns>The hash of the underlying object.</returns>
+        /// <param name="other">An object to compare with this object.</param>
+        /// <returns>true if the current object is equal to the other parameter; otherwise, false.
+        /// </returns>
+        public bool Equals(ObjectEnum other) =>
+            other != null &&
+            Value.Equals(other.Value);
+
+        /// <summary>
+        /// Returns the hash code of the underlying value.
+        /// </summary>
+        /// <returns>The hash code of the underlying value.</returns>
         public override int GetHashCode() => Value.GetHashCode();
+
+        /// <summary>
+        /// Returns the <see cref="string"/> representation of the underlying object.
+        /// Calls <see cref="object.ToString"/> on the underlying object.
+        /// </summary>
+        /// <returns>The <see cref="string"/> representation of the underlying object.</returns>
+        public override string ToString() => Value.ToString();
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+        public static bool operator ==(ObjectEnum left, ObjectEnum right) =>
+            EqualityComparer<object>.Default.Equals(left?.Value, right?.Value);
+
+        public static bool operator !=(ObjectEnum left, ObjectEnum right) => !(left == right);
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
     }
 }
