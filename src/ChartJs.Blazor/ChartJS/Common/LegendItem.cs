93e5584f378646435acb2535d83329ca30b50f3f
Joelius300 <52202086+Joelius300@users.noreply.github.com>
2020-03-29 21:06:52 +0200

    Add DatasetIndex to LegendItem, clarify/improve summaries

--- src/ChartJs.Blazor/ChartJS/Common/LegendItem.cs
+++ src/ChartJs.Blazor/ChartJS/Common/LegendItem.cs
@@ -7,16 +7,31 @@ using System.Text;
 namespace ChartJs.Blazor.ChartJS.Common
 {
     /// <summary>
-    /// The model of the legend items which are displayed in the chart.
-    /// <para>As per documentation here https://www.chartjs.org/docs/latest/configuration/legend.html#legend-item-interface </para>
+    /// The model of the legend items which are displayed in the chart-legend.
+    /// <para>
+    /// As per documentation here:
+    /// <a href="https://www.chartjs.org/docs/latest/configuration/legend.html#legend-item-interface"/>
+    /// </para>
     /// </summary>
     public class LegendItem
     {
         /// <summary>
-        /// The index of the dataset this legend item corresponds to. DO NOT set this value when returning an instance of this class to ChartJs.
+        /// Gets or sets the index of the dataset this legend item corresponds to.
+        /// DO NOT set this value when returning an instance of this class to chart.js.
         /// Only use this property when retrieving the index in a legend-event.
+        /// This value might not be set in charts that have legend labels per value
+        /// (instead of per dataset) like pie-chart.
         /// </summary>
-        public int? Index { get; set; } // TODO in 2.9 this changes to DatasetIndex
+        public int? DatasetIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the index of the value this legend item corresponds to.
+        /// DO NOT set this value when returning an instance of this class to chart.js.
+        /// Only use this property when retrieving the index in a legend-event.
+        /// This value is only set in charts that have legend labels per value
+        /// (instead of per dataset) like pie-chart.
+        /// </summary>
+        public int? Index { get; set; }
 
         /// <summary>
         /// Gets or sets the label-text that will be displayed.
@@ -30,7 +45,8 @@ namespace ChartJs.Blazor.ChartJS.Common
         public string FillStyle { get; set; }
 
         /// <summary>
-        /// If true, this item represents a hidden dataset. Label will be rendered with a strike-through effect.
+        /// Gets or sets a value indicating whether or not this legend-item represents a
+        /// hidden dataset. Label will be rendered with a strike-through effect if <see langword="true"/>.
         /// </summary>
         public bool? Hidden { get; set; }
 
@@ -40,12 +56,16 @@ namespace ChartJs.Blazor.ChartJS.Common
         public BorderCapStyle LineCap { get; set; }
 
         /// <summary>
-        /// Gets or sets the line dash segments for the legend box border. Details on https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/setLineDash.
+        /// Gets or sets the line dash segments for the legend box border.
+        /// Details on
+        /// <a href="https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/setLineDash"/>.
         /// </summary>
         public double[] LineDash { get; set; }
 
         /// <summary>
-        /// Gets or sets the line dash offset. Details on https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/lineDashOffset.
+        /// Gets or sets the line dash offset.
+        /// Details on
+        /// <a href="https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/lineDashOffset"/>.
         /// </summary>
         public double? LineDashOffset { get; set; }
 
@@ -66,12 +86,14 @@ namespace ChartJs.Blazor.ChartJS.Common
         public string StrokeStyle { get; set; }
 
         /// <summary>
-        /// Gets or sets the <see cref="PointStyle"/> of the legend box (only used if <see cref="LegendLabels.UsePointStyle"/> is true). 
+        /// Gets or sets the <see cref="PointStyle"/> of the legend box
+        /// (only used if <see cref="LegendLabels.UsePointStyle"/> is <see langword="true"/>).
         /// </summary>
         public PointStyle PointStyle { get; set; }
 
         /// <summary>
-        /// Gets or sets the rotation of the point in degrees (only used if <see cref="LegendLabels.UsePointStyle"/> is true).
+        /// Gets or sets the rotation of the point in degrees
+        /// (only used if <see cref="LegendLabels.UsePointStyle"/> is <see langword="true"/>).
         /// </summary>
         public double? Rotation { get; set; }
     }
