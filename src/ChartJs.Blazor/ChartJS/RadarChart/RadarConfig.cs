62032791574b4faa228951b3ccb662c002118737
Joelius300 <bstlng321@gmail.com>
2020-06-02 08:22:16 +0200

    Fix radar-chart
    - Scale changed to LinearRadialAxis
    - Update summaries
    - Fixes #110

--- src/ChartJs.Blazor/ChartJS/RadarChart/RadarConfig.cs
+++ src/ChartJs.Blazor/ChartJS/RadarChart/RadarConfig.cs
@@ -3,8 +3,14 @@ using ChartJs.Blazor.ChartJS.Common.Enums;
 
 namespace ChartJs.Blazor.ChartJS.RadarChart
 {
+    /// <summary>
+    /// Represents the config for a radar chart.
+    /// </summary>
     public class RadarConfig : ConfigBase<RadarOptions, RadarData>
     {
+        /// <summary>
+        /// Creates a new instance of <see cref="RadarConfig"/>.
+        /// </summary>
         public RadarConfig() : base(ChartType.Radar) { }
     }
 }
