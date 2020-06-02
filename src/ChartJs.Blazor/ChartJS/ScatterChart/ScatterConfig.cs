0397babafba9db722bc61a36b8c51f1c310c8ca5
Joelius300 <bstlng321@gmail.com>
2020-06-02 08:56:46 +0200

    Rework scatter-chart
    - Remove old classes
    - Use LineOptions and add appropriate comment

--- src/ChartJs.Blazor/ChartJS/ScatterChart/ScatterConfig.cs
+++ src/ChartJs.Blazor/ChartJS/ScatterChart/ScatterConfig.cs
@@ -3,8 +3,19 @@ using ChartJs.Blazor.ChartJS.Common.Enums;
 
 namespace ChartJs.Blazor.ChartJS.ScatterChart
 {
-    public class ScatterConfig : ConfigBase<ScatterOptions, ScatterData>
+    /// <summary>
+    /// Represents the config for a scatter chart.
+    /// <para>
+    /// Scatter charts are based on basic line charts with the x axis changed to a
+    /// <see cref="Common.Axes.LinearCartesianAxis"/> (unless otherwise specified).
+    /// Therefore, many configuration options are from the line chart.
+    /// </para>
+    /// </summary>
+    public class ScatterConfig : ConfigBase<LineChart.LineOptions, ScatterData>
     {
+        /// <summary>
+        /// Creates a new instance of <see cref="ScatterConfig"/>.
+        /// </summary>
         public ScatterConfig() : base(ChartType.Scatter) { }
     }
 }
