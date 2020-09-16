a28285cf269db3b4c386a4b2a0a0d901d7636d69
Joelius300 <bstlng321@gmail.com>
2020-09-16 21:28:04 +0200

    UI scaffolding

---
+++ ChartJs.Blazor.Samples/Shared/SampleCategory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChartJs.Blazor.Samples.Shared
+{
+    public class SampleCategory
+    {
+        public string Title { get; set; }
+        public IList<Sample> Samples { get; } = new List<Sample>();
+    }
+}
