d26ab5aba8b19f5c2f8c1655bfcc6cd41a154523
Joelius300 <bstlng321@gmail.com>
2020-09-16 21:28:04 +0200

    UI scaffolding

---
+++ ChartJs.Blazor.Samples/Shared/ISamplesProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChartJs.Blazor.Samples.Shared
+{
+    public interface ISamplesProvider
+    {
+        /// <summary>
+        /// Returns an enumeration of sample categories each containing their respective samples.
+        /// </summary>
+        IEnumerable<SampleCategory> GetSamples();
+    }
+}
---
+++ ChartJs.Blazor.Samples/Shared/Sample.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChartJs.Blazor.Samples.Shared
+{
+    public class Sample
+    {
+        public string Title { get; set; }
+        public string Path { get; set; }
+    }
+}
