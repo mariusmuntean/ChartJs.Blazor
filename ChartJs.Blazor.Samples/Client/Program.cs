b7c07a7dcabd699782d85268b894f634f85dfd12
Joelius300 <bstlng321@gmail.com>
2020-09-30 21:39:55 +0200

    Refactor the blazor dual mode switch
    It's weird, I don't know if this is right

--- ChartJs.Blazor.Samples/Client/Program.cs
+++ ChartJs.Blazor.Samples/Client/Program.cs
@@ -20,6 +20,7 @@ namespace ChartJs.Blazor.Samples.Client
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<ISamplesProvider, ApiSamplesProvider>();
+            builder.Services.AddSingleton<DualModeSerivce>();
 
             await builder.Build().RunAsync();
         }
