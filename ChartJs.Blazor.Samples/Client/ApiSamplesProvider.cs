289d5fa0a3a3478eb27915fbb4402292de014a35
Joelius300 <bstlng321@gmail.com>
2020-09-30 21:38:53 +0200

    Load samples from file

---
+++ ChartJs.Blazor.Samples/Client/ApiSamplesProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using ChartJs.Blazor.Samples.Shared;
+
+namespace ChartJs.Blazor.Samples.Client
+{
+    /// <summary>
+    /// Reads the samples from the json file located in the
+    /// WebRoot by fetching it from the server.
+    /// </summary>
+    public class ApiSamplesProvider : ISamplesProvider
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiSamplesProvider(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task<IEnumerable<SampleCategory>> GetSamples()
+        {
+            return _httpClient.GetFromJsonAsync<IEnumerable<SampleCategory>>(SamplesProvider.SamplesFileName);
+        }
+    }
+}
---
+++ ChartJs.Blazor.Samples/Server/JsonFileSamplesProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ChartJs.Blazor.Samples.Shared;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ChartJs.Blazor.Samples.Server
+{
+    /// <summary>
+    /// Reads the samples from the json file located in the
+    /// WebRoot directly on the server.
+    /// </summary>
+    public class JsonFileSamplesProvider : ISamplesProvider
+    {
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public JsonFileSamplesProvider(IWebHostEnvironment hostEnvironment)
+        {
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public async Task<IEnumerable<SampleCategory>> GetSamples()
+        {
+            string path = Path.Combine(_hostEnvironment.WebRootPath, SamplesProvider.SamplesFileName);
+            using FileStream file = File.OpenRead(path);
+
+            // Cannot return the Task directly because then the file
+            // will be closed once the deserialization actually takes place
+            return await JsonSerializer.DeserializeAsync<IEnumerable<SampleCategory>>(file);
+        }
+    }
+}
