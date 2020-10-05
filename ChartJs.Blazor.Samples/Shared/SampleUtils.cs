e2e5a53a2cc0d1fe6d60b325242b0edcbe979c21
Joelius300 <bstlng321@gmail.com>
2020-10-05 22:02:44 +0200

    Use Color objects instead of strings
    This allows for easier manipulation before use

--- ChartJs.Blazor.Samples/Shared/SampleUtils.cs
+++ ChartJs.Blazor.Samples/Shared/SampleUtils.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Drawing;
 using System.Text;
 
 namespace ChartJs.Blazor.Samples.Shared
@@ -12,18 +13,20 @@ namespace ChartJs.Blazor.Samples.Shared
 
         public static class ChartColors
         {
-            public static IReadOnlyList<string> All { get; } = new string[7]
+            private static readonly Lazy<IReadOnlyList<Color>> _all = new Lazy<IReadOnlyList<Color>>(() => new Color[7]
             {
                 Red, Orange, Yellow, Green, Blue, Purple, Grey
-            };
+            });
 
-            public const string Red = "rgb(255, 99, 132)";
-            public const string Orange = "rgb(255, 159, 64)";
-            public const string Yellow = "rgb(255, 205, 86)";
-            public const string Green = "rgb(75, 192, 192)";
-            public const string Blue = "rgb(54, 162, 235)";
-            public const string Purple = "rgb(153, 102, 255)";
-            public const string Grey = "rgb(201, 203, 207";
+            public static IReadOnlyList<Color> All => _all.Value;
+
+            public static readonly Color Red = Color.FromArgb(255, 99, 132);
+            public static readonly Color Orange = Color.FromArgb(255, 159, 64);
+            public static readonly Color Yellow = Color.FromArgb(255, 205, 86);
+            public static readonly Color Green = Color.FromArgb(75, 192, 192);
+            public static readonly Color Blue = Color.FromArgb(54, 162, 235);
+            public static readonly Color Purple = Color.FromArgb(153, 102, 255);
+            public static readonly Color Grey = Color.FromArgb(201, 203, 207);
         }
 
         public static IReadOnlyList<string> Months { get; } = new ReadOnlyCollection<string>(new[]
