9b8851eca69b50e60944fd1c698a6c20fa7ebdb8
Joelius300 <bstlng321@gmail.com>
2020-10-20 21:31:28 +0200

    Rewrite summary of AxisType.Category

--- src/ChartJs.Blazor/Common/Enums/AxisType.cs
+++ src/ChartJs.Blazor/Common/Enums/AxisType.cs
@@ -20,16 +20,19 @@
         /// </summary>
         public static AxisType Logarithmic => new AxisType("logarithmic");
 
-
-        // TODO: rewrite this summary and add references to the correct properties
         /// <summary>
-        /// If global configuration is used, labels are drawn from one of the label arrays included in the chart data.
-        /// If only data.labels is defined, this will be used. If data.xLabels is defined and the axis is horizontal, this will be used.
-        /// Similarly, if data.yLabels is defined and the axis is vertical, this property will be used.
+        /// If global configuration is used, labels are drawn from one of the label arrays included in the chart data (<see cref="ChartData"/>).
+        /// If only <see cref="ChartData.Labels"/> is defined, this will be used. If <see cref="ChartData.XLabels"/> is defined and the axis is horizontal, this will be used.
+        /// Similarly, if <see cref="ChartData.YLabels"/> is defined and the axis is vertical, this property will be used.
         /// Using both xLabels and yLabels together can create a chart that uses strings for both the X and Y axes.
-        /// Specifying any of the settings above defines the x axis as type: <see cref="AxisType.Category" /> if not defined otherwise.
-        /// For more fine-grained control of category labels it is also possible to add labels as part of the category axis definition.
-        /// Doing so does not apply the global defaults.
+        /// Specifying any of the settings above defines the x axis as type: <see cref="Category" /> if not defined otherwise.
+        /// <para>
+        /// Note: The axes in this library are strongly typed and always contain a type so this inference only applies if you don't provide an axis in the options.
+        /// </para>
+        /// <para>
+        /// For more fine-grained control of category labels it is also possible to add labels as part of the category axis definition (<see cref="Axes.Ticks.CategoryTicks.Labels"/>).
+        /// Doing so does not apply the global defaults for the chart.
+        /// </para>
         /// <para>See <a href="https://www.chartjs.org/docs/latest/axes/cartesian/category.html">here (Chart.js)</a>.</para>
         /// </summary>
         public static AxisType Category => new AxisType("category");
