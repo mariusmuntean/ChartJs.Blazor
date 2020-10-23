609a6e5bf8c70cbeea8473bc9d9235c1d6b75e3f
Joelius300 <bstlng321@gmail.com>
2020-10-23 20:43:37 +0200

    Add gradient sample to show workaround for #158

---
+++ ChartJs.Blazor.Samples/Server/wwwroot/workaround-gradient.js
@@ -0,0 +1,13 @@
+﻿window.workaroundGradient = function (canvasId) {
+    let canvas = document.getElementById(canvasId);
+    let ctx = canvas.getContext("2d");
+    let gradient = ctx.createLinearGradient(0, 0, 0, canvas.clientHeight);
+        gradient.addColorStop(0, 'rgba(250,174,50,1)');
+        gradient.addColorStop(1, 'rgba(250,174,50,0)');
+
+    let chart = window.ChartJsInterop.BlazorCharts.get(canvasId);
+    let datasets = chart.config.data.datasets;
+    for (let dataset of datasets) {
+        dataset.backgroundColor = gradient;
+    }
+}
