63603e4fc76172b16688f02866200695ab007d15
Akira Yamamoto <3007213+akirayamamoto@users.noreply.github.com>
2023-12-13 10:40:07 +1100

    Add layout padding support. (#213)

---
+++ src/ChartJs.Blazor/Common/Padding.cs
@@ -0,0 +1,14 @@
+﻿namespace ChartJs.Blazor.Common
+{
+    /// <summary>
+    /// The padding to add to the chart.
+    /// <para>As per documentation <a href="https://www.chartjs.org/docs/latest/general/padding.html">here (Chart.js)</a>.</para>
+    /// </summary>
+    public class Padding
+    {
+        public int Left { get; set; }
+        public int Right { get; set; }
+        public int Top { get; set; }
+        public int Bottom { get; set; }
+    }
+}
